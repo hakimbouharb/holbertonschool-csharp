3cff864128b30b592f3c88687637851d7a06dd36
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 <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>
2024-05-05 14:13:03 +0100

    task 3

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 #initiate new project 
-dotnet new console -o 1-new_project && cd 1-new_project && dotnet build dotnet run 
+dotnet new console -o 1-new_project && cd 1-new_project && dotnet build && dotnet run 
