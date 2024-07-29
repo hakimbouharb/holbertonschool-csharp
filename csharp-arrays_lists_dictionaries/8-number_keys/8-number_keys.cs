41d128456228f951ea383636cc595aabd91b4084
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 <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>
2024-07-29 13:47:53 +0100

    number key 3

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+
+        int countKey = 0;
+        foreach (var item in myDict)
+            countKey++;
+        return countKey;
+    }
+}
