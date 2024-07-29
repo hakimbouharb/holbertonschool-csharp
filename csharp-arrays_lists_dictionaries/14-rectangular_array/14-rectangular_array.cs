b70f41a208042cdfba2ee78d769c98a5e7fde060
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 <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>
2024-07-29 14:05:47 +0100

    rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] multiArray = new int [5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    multiArray[i, j] = 0;
+                }
+            }
+            multiArray[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(multiArray[i, j]);
+                    if (j != 4)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
