5f8b149211792267c23de939af4a4e76e878aa36
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 <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>
2024-08-09 14:21:25 +0100

    linkedslist get node

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (var item in myLList)
+        {
+            if (index == n)
+                return item;
+            index++;         
+        }
+        return 0;
+    }
+}
