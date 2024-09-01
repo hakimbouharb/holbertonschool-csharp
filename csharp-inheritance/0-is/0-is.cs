fceb2f7e3126d22dc688839c972a58b475b8dbe0
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 <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>
2024-09-01 20:19:04 +0100

    0 is

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+
+///<summary> Obj class.
+///</summary>
+public class Obj
+{
+    ///<summary> Check if obj of type int or not.
+    ///</summary>
+    ///<param name="obj">Object to check.</param>
+    ///<return>True if obj is of type int false otherwise.</return>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return true;
+        return false;
+    }
+}
